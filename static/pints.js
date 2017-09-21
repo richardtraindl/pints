2cf792dc4ce86e82c7cd03cbdec38047c9a7e2fc
Richard Traindl <richardtraindl@gmail.com>
2017-09-21 11:08:46 +0200

    Update pints.js

--- static/pints.js
+++ static/pints.js
@@ -143,6 +143,7 @@ var calc_distance = function(long, lat, long1, lat1){
                   var pub = ranked_pub[1];
 
                   var lst = "<li>";
+                  lst = "<span style='width: 90%; text-align: top;'>";
                   // lst += "<div class='table-cell' style='width: 95%'>";
                   lst += "<a href='/pints/pub.html' id='" + pub.name + "'>";
                   lst += pub.name + " ";
@@ -160,9 +161,10 @@ var calc_distance = function(long, lat, long1, lat1){
                     lst += mail.mail + " ";
                   }; */
                   lst += "</a>";
+                  lst += "</span>";
 
                   // lst += "<div class='table-cell' style='width: 5%'>";
-                  lst += "<span>";
+                  lst += "<span style='width: 10%; text-align: top;'>";
                   lst += distance;
                   lst += "</span>";
                   lst += "</li>";
