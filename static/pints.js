b2b08dfb1149977b351a57df3814d687fa9b996f
Richard Traindl <richardtraindl@gmail.com>
2017-09-29 15:23:30 +0200

    Update pints.js

--- static/pints.js
+++ static/pints.js
@@ -54,6 +54,16 @@
 
   function show_pubs(count){
     
+    // map
+    var mymap = L.map('mapid').setView([location.latitude, location.longitude], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                   '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                   'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets'
+    }).addTo(mymap);
+
     var location = { status: null, latitude: null, longitude: null };
     get_location(location);
 
@@ -81,7 +91,7 @@
         return false;
       }
 
-      $.getJSON('/data/pubs.json', function(data){
+     $.getJSON('/data/pubs.json', function(data){
         var rpubs = new Array();
 
         $.each(data, function(index, pub){
@@ -96,16 +106,6 @@
         };
 
         rpubs.sort(compare_distance);
-
-        // map
-        var mymap = L.map('mapid').setView([location.latitude, location.longitude], 13);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-          maxZoom: 18,
-          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                       '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                       'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-          id: 'mapbox.streets'
-        }).addTo(mymap);
 
         // list
         $('#pub-list').html('');
