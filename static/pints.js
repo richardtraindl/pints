0c98473b8a1cc8797518d2c11dd2b4db4fe7b4a4
Richard Traindl <richardtraindl@gmail.com>
2017-09-29 11:12:32 +0200

    Update pints.js

--- static/pints.js
+++ static/pints.js
@@ -59,6 +59,7 @@
 
     setTimeout(function(){ 
       console.log(" stat: " + location.status + " lat: " + location.latitude + " lon: " + location.longitude);
+
       if(location.status == null){
         $('.msg').html('');
         var msg = "<p>Navigation did work. <br>Please check GPS-support and reload page!</p>";
@@ -80,107 +81,104 @@
         return false;
       }
     
-    var mymap = L.map('mapid').setView([location.latitude, location.longitude], 13);
+      var mymap = L.map('mapid').setView([location.latitude, location.longitude], 13);
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-      maxZoom: 18,
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                   '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                   'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      id: 'mapbox.streets'
-    }).addTo(mymap);
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.streets'
+      }).addTo(mymap);
 
-    $.getJSON('/data/pubs.json', function(data){
-      var rpubs = new Array();
+      $.getJSON('/data/pubs.json', function(data){
+        var rpubs = new Array();
 
-      $.each(data, function(index, pub){
-        var distance = calc_distance(location.longitude, location.latitude, pub.longitude, pub.latitude);
-        rpubs.push([distance, pub]);
+        $.each(data, function(index, pub){
+          var distance = calc_distance(location.longitude, location.latitude, pub.longitude, pub.latitude);
+          rpubs.push([distance, pub]);
+        });
+
+        function compare_distance(a, b){
+          if (a[0] < b[0]) return -1;
+          if (a[0] > b[0]) return 1;
+          return 0;
+        };
+
+        rpubs.sort(compare_distance);
+
+        $('#pub-list').html('');
+        $('#pub-list').append("<ul></ul>");
+
+        var cnt = 0;
+        for(let rpub of rpubs){
+          cnt += 1;
+          if(cnt <= count){
+            var distance = rpub[0];
+            var pub = rpub[1];
+
+            var lst = "<li>";
+            lst += "<div style='display: flex; align-items: center'>";
+
+            lst += "<div style='width: 4%; margin: 0px;'>";
+            lst += "<span style='font-size: 20px'>" + cnt.toString() + "</span>";
+            lst += "</div>"; // cell
+
+            lst += "<div style='width: 20%; margin: 0px;'>";
+            if(pub.website.length > 0){
+              lst += "<a href='" + pub.website + "' target='_blank' style='font-weight: bold'>" + pub.name + "</a><br>";
+            }
+            else{
+              lst += "<span style='font-weight: bold'>" + pub.name + "</span><br>";
+            }
+            for(category of pub.categories){
+              lst += category + " ";
+            }
+            if(pub.food.length > 0){
+              lst += "<br>" + pub.food;
+            }
+            lst += "</div>"; // cell
+
+            lst += "<div class='div-cell' style='width: 66%; margin: 0px;'>";
+            if(pub.features.length > 0){
+              lst += pub.features + "<br>";
+            }
+            if(pub.open.length > 0){
+              lst += pub.open + "<br>";
+            }
+            for(tel of pub.tel){
+              lst += "<a href='tel:" + tel + "' style='color: Black'>" + tel + "</a> &nbsp; ";
+            }
+            lst += "</div>"; // cell
+
+            lst += "<div class='div-cell' style='width: 10%; margin: 0px;'>";
+            lst += distance + " km";
+            lst += "</div>"; // cell
+            lst += "</div>"; // row
+            lst += "</li>";
+
+            $('#pub-list').find('ul').append(lst);
+
+            var pub1 = L.marker([pub.latitude, pub.longitude]).addTo(mymap);
+            var popup = cnt.toString() + ") &nbsp;"
+            popup += pub.name + ", ";
+            for(category of pub.categories){
+              popup += category + " ";
+            }
+            popup += "<br>" + pub.address + "<br>"
+            for(tel of pub.tel){
+              popup += tel + " ";
+            }
+            if(pub.open.length > 0){
+              popup += "<br>" + pub.open;
+            }
+
+            pub1.bindPopup(popup);
+          }
+        } 
+        var userlocation = L.marker([location.latitude, location.longitude]).addTo(mymap);
+        userlocation.bindPopup("Here you are!").openPopup();
       });
-
-      function compare_distance(a, b){
-        if (a[0] < b[0]) return -1;
-        if (a[0] > b[0]) return 1;
-        return 0;
-      };
-
-      rpubs.sort(compare_distance);
-
-      $('#pub-list').html('');
-      $('#pub-list').append("<ul></ul>");
-
-      var cnt = 0;
-      for(let rpub of rpubs){
-        cnt += 1;
-        if(cnt <= count){
-          var distance = rpub[0];
-          var pub = rpub[1];
-
-          var lst = "<li>";
-          lst += "<div style='display: flex; align-items: center'>";
-
-          lst += "<div style='width: 4%; margin: 0px;'>";
-          lst += "<span style='font-size: 20px'>" + cnt.toString() + "</span>";
-          lst += "</div>"; // cell
-        
-          lst += "<div style='width: 20%; margin: 0px;'>";
-          if(pub.website.length > 0){
-            lst += "<a href='" + pub.website + "' target='_blank' style='font-weight: bold'>" + pub.name + "</a><br>";
-          }
-          else{
-            lst += "<span style='font-weight: bold'>" + pub.name + "</span><br>";
-          }
-          for(category of pub.categories){
-            lst += category + " ";
-          }
-          if(pub.food.length > 0){
-            lst += "<br>" + pub.food;
-          }
-          lst += "</div>"; // cell
-
-          lst += "<div class='div-cell' style='width: 66%; margin: 0px;'>";
-          if(pub.features.length > 0){
-            lst += pub.features + "<br>";
-          }
-          if(pub.open.length > 0){
-            lst += pub.open + "<br>";
-          }
-          for(tel of pub.tel){
-            lst += "<a href='tel:" + tel + "' style='color: Black'>" + tel + "</a> &nbsp; ";
-          }
-          lst += "</div>"; // cell
-
-          lst += "<div class='div-cell' style='width: 10%; margin: 0px;'>";
-          lst += distance + " km";
-          lst += "</div>"; // cell
-          lst += "</div>"; // row
-          lst += "</li>";
-
-          $('#pub-list').find('ul').append(lst);
-          
-          var pub1 = L.marker([pub.latitude, pub.longitude]).addTo(mymap);
-          var popup = cnt.toString() + ") &nbsp;"
-          popup += pub.name + ", ";
-          for(category of pub.categories){
-            popup += category + " ";
-          }
-          popup += "<br>" + pub.address + "<br>"
-          for(tel of pub.tel){
-            popup += tel + " ";
-          }
-          if(pub.open.length > 0){
-            popup += "<br>" + pub.open;
-          }
-
-          pub1.bindPopup(popup);
-
-        }
-      } 
-
-     }, 3000);
-    });
-
-    var userlocation = L.marker([location.latitude, location.longitude]).addTo(mymap);
-    userlocation.bindPopup("Here you are!").openPopup();
+    }, 3000);
   };
 
